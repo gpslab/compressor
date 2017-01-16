36181db19152ad2c970b6659b899caf35b3428a1
Peter Gribanov <wcode404@gmail.com>
2017-01-16 19:21:21 +0300

    test DummyCompressor

---
+++ tests/DummyCompressorTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Pkvs package
+ *
+ * @package Pkvs
+ * @author  Peter Gribanov <pgribanov@1tv.com>
+ */
+namespace GpsLab\Component\Compressor\Tests;
+
+use GpsLab\Component\Compressor\DummyCompressor;
+
+class DummyCompressorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DummyCompressor
+     */
+    private $compressor;
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
+     * @var string
+     */
+    private $source = '';
+
+    /**
+     * @var string
+     */
+    private $target = '';
+
+    protected function setUp()
+    {
+        $this->source = tempnam(sys_get_temp_dir(), 'test');
+        $this->target = tempnam(sys_get_temp_dir(), 'test');
+
+        $this->file = new File();
+        $this->compressor = new DummyCompressor();
+    }
+
+    protected function tearDown()
+    {
+        if (file_exists($this->source)) {
+            unlink($this->source);
+        }
+        if (file_exists($this->target)) {
+            unlink($this->target);
+        }
+    }
+
+    public function testCompressDoNothing()
+    {
+        $this->assertTrue($this->compressor->compress($this->source));
+    }
+
+    public function testCompress()
+    {
+        $this->file->put($this->source);
+
+        $this->assertTrue($this->compressor->compress($this->source, $this->target));
+        $this->assertTrue(file_exists($this->target));
+        $this->assertTrue($this->file->equals($this->target));
+    }
+
+    public function testUncompressDoNothing()
+    {
+        $this->assertTrue($this->compressor->uncompress($this->source, $this->source));
+    }
+
+    public function testUncompress()
+    {
+        $this->file->put($this->source);
+
+        $this->assertTrue($this->compressor->uncompress($this->source, $this->target));
+        $this->assertTrue(file_exists($this->target));
+        $this->assertTrue($this->file->equals($this->target));
+    }
+}
